fb41a132f427ac5c5b94ed3b71489f69bfad698e
mickael bitane <bitane.mickael@gmail.com>
2024-11-11 14:52:36 +0100

    02

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,7 +6,16 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    
-  end
+    firstanswer = 'great'
+    secondanswer = 'Silly question, get dressed and go to work!'
+    thirdanswer = 'I dont care, get dressed and go to work!'
 
+    if params[:question].end_with?('?')
+      @coachanswer = secondanswer
+      elsif params[:question] == 'i am going to work'
+        @coachanswer = firstanswer
+      else
+        @coachanswer = thirdanswer
+    end
+  end
 end
